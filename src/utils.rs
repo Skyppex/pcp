e426d23a990206a50abc16d22843c995024031a6
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-04-08 14:01:17 +0200

    feat: copy single file with threads

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use walkdir::DirEntry;
 
-pub fn calculate_total_size(files: &Vec<DirEntry>) -> u64 {
+pub fn calculate_total_size(files: &[DirEntry]) -> u64 {
     files
         .iter()
         .map(|src| std::fs::metadata(src.path()).map(|m| m.len()).unwrap_or(0))
