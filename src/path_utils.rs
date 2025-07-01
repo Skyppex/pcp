07f73f4eb8ae0ad0d679d6bc8e2ee7e3bde0b832
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-07-02 00:33:11 +0200

    fix(utils): fix path utils and tests

--- src/path_utils.rs
+++ src/path_utils.rs
@@ -22,20 +22,21 @@ pub fn get_path(path: impl AsRef<Path>) -> Result<PathBuf> {
                 .parent()
                 .ok_or(std::io::Error::from(ErrorKind::NotFound))?;
 
-            let mut path = Path::new(&p[3..]);
+            let mut path = String::from(&p[3..]);
 
             while path.starts_with("..") {
                 current_dir = current_dir
                     .parent()
                     .ok_or(std::io::Error::from(ErrorKind::NotFound))?;
-                path = Path::new(&path.to_str().unwrap()[3..]);
+                path = String::from(&path[3..]);
             }
 
             current_dir.join(path)
         }
-        p if p.starts_with("./") || p.starts_with(".\\") => std::env::current_dir()?.join(&p[2..]),
+        p if p.starts_with("./") || p.starts_with(r".\") => std::env::current_dir()?.join(&p[2..]),
         p => {
             let path = Path::new(p);
+
             if !Path::has_root(path) {
                 return Ok(std::env::current_dir()?.join(p));
             }
@@ -53,7 +54,7 @@ mod tests {
 
     #[test]
     fn test_get_full_path() {
-        let path = "~\\.cargo".to_string();
+        let path = r"~\.cargo".to_string();
         let binding = get_path(&path).unwrap();
         let full_path = binding.to_str().unwrap();
         let binding = dirs::home_dir().unwrap().join(".cargo");
@@ -72,18 +73,30 @@ mod tests {
     }
 
     #[test]
+    #[cfg(target_os = "windows")]
     fn test_get_full_path_absolute() {
-        let path = "C:\\Users\\user\\.cargo".to_string();
+        let path = r"C:\Users\user\.cargo".to_string();
         let binding = get_path(&path).unwrap();
         let full_path = binding.to_str().unwrap();
-        let binding = Path::new("C:\\Users\\user\\.cargo").to_path_buf();
+        let binding = Path::new(r"C:\Users\user\.cargo").to_path_buf();
+        let expected = binding.to_str().unwrap();
+        assert_eq!(full_path, expected);
+    }
+
+    #[test]
+    #[cfg(target_os = "linux")]
+    fn test_get_full_path_absolute() {
+        let path = "/home/user/.cargo".to_string();
+        let binding = get_path(&path).unwrap();
+        let full_path = binding.to_str().unwrap();
+        let binding = Path::new(r"/home/user/.cargo").to_path_buf();
         let expected = binding.to_str().unwrap();
         assert_eq!(full_path, expected);
     }
 
     #[test]
     fn test_get_double_dottet_path() {
-        let path = "..\\src".to_string();
+        let path = r"..\src".to_string();
         let binding = get_path(&path).unwrap();
         let full_path = binding.to_str().unwrap();
         let binding = std::env::current_dir()
@@ -97,7 +110,7 @@ mod tests {
 
     #[test]
     fn test_get_double_dottet_twice_path() {
-        let path = "..\\..\\src".to_string();
+        let path = r"..\..\src".to_string();
         let binding = get_path(&path).unwrap();
         let full_path = binding.to_str().unwrap();
         let binding = std::env::current_dir()
