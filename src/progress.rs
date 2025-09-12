6d41c4ec19c8974f7d68aa1589c0bb02c80e3534
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-09-12 16:46:12 +0200

    feat(progress): add bytes-level progress tracking api

--- src/progress.rs
+++ src/progress.rs
@@ -1,8 +1,8 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     ffi::OsString,
     fs::{File, OpenOptions},
-    io::{Read, Write},
+    io::{Read, Seek, Write},
     path::{Path, PathBuf},
 };
 
@@ -10,22 +10,32 @@ use os_str_bytes::{OsStrBytes, OsStringBytes};
 
 const PROGRESS_DIR: &'static str = ".pcp";
 const COMPLETED_FILE_NAME: &'static str = ".pcp-completed.pcp";
+const PROGRESS_EXT: &'static str = ".pcp";
+const NEW_LINE_BUFFER: usize = 128;
+const BLANK_SPACE_CHAR: &'static str = " "; // space
 
 pub struct CompletionTracker {
-    file: Option<File>,
+    completed_file: Option<File>,
     dest: Option<PathBuf>,
-    path: Option<PathBuf>,
-    len: u64,
+    completed_path: Option<PathBuf>,
+    completed_len: u64,
+    progress_files: HashMap<PathBuf, Progress>,
+}
+
+struct Progress {
+    file: File,
+    total_bytes: usize,
 }
 
 impl CompletionTracker {
     pub fn open(dest_dir: impl AsRef<Path>, enabled: bool) -> std::io::Result<CompletionTracker> {
         if !enabled {
             return Ok(CompletionTracker {
-                file: None,
+                completed_file: None,
                 dest: None,
-                path: None,
-                len: 0,
+                completed_path: None,
+                completed_len: 0,
+                progress_files: HashMap::new(),
             });
         }
 
@@ -47,15 +57,16 @@ impl CompletionTracker {
         let len = file.metadata()?.len();
 
         Ok(CompletionTracker {
-            file: Some(file),
+            completed_file: Some(file),
             dest: Some(dest_dir.to_path_buf()),
-            path: Some(completed_file_path),
-            len,
+            completed_path: Some(completed_file_path),
+            completed_len: len,
+            progress_files: HashMap::new(),
         })
     }
 
     pub fn read(&mut self) -> HashSet<OsString> {
-        let Some(file) = &mut self.file else {
+        let Some(file) = &mut self.completed_file else {
             return HashSet::new();
         };
 
@@ -75,7 +86,7 @@ impl CompletionTracker {
     }
 
     pub fn add_completed(&mut self, completed: impl AsRef<Path>) -> std::io::Result<()> {
-        let Some(file) = &mut self.file else {
+        let Some(file) = &mut self.completed_file else {
             return Ok(());
         };
 
@@ -92,11 +103,76 @@ impl CompletionTracker {
     }
 
     pub fn remove(self) -> std::io::Result<()> {
-        let Some(path) = self.path else {
+        let Some(path) = self.completed_path else {
             return Ok(());
         };
 
         std::fs::remove_file(path)
+    }
+
+    pub fn add_progress_file(
+        &mut self,
+        file_name: impl AsRef<Path>,
+        total_bytes: usize,
+    ) -> std::io::Result<()> {
+        let Some(dest) = &self.dest else {
+            return Ok(());
+        };
+
+        let file_path = dest.join(file_name.as_ref()).join(PROGRESS_EXT);
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .read(true)
+            .create(true)
+            .truncate(false)
+            .open(&file_path)?;
+
+        file.write_all(
+            format!(
+                "{}{}\n{}",
+                "0",
+                BLANK_SPACE_CHAR.repeat(NEW_LINE_BUFFER - 1),
+                total_bytes
+            )
+            .as_bytes(),
+        )?;
+
+        let progress = Progress { file, total_bytes };
+
+        let _ = self.progress_files.insert(file_path, progress);
+
+        Ok(())
+    }
+
+    pub fn write_progress(
+        &mut self,
+        file_name: impl AsRef<Path>,
+        current_bytes: usize,
+    ) -> std::io::Result<()> {
+        let Some(dest) = &self.dest else {
+            return Ok(());
+        };
+
+        let file_path = dest.join(file_name).join(PROGRESS_EXT);
+        let progress = self
+            .progress_files
+            .get_mut(&file_path)
+            .ok_or_else(|| std::io::ErrorKind::NotFound)?;
+
+        let file = &mut progress.file;
+        let data = current_bytes.to_string();
+        let bytes = data.as_bytes();
+
+        file.seek(std::io::SeekFrom::Start(0))?;
+        file.write_all(bytes)?;
+        file.sync_data()?;
+
+        Ok(())
+    }
+
+    pub fn remove_progress_file(&mut self, file_name: impl AsRef<Path>) -> bool {
+        self.progress_files.remove(file_name.as_ref()).is_some()
     }
 }
 
